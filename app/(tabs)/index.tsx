49b365062fb2515e2522641a6bddd2aba80752e9
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-05-21 09:02:46 +0000

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text> I am developing a Akccept app </Text>
+    </View>
+  );
+}
+
